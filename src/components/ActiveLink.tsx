a84cc9f7e56f510e630dcb59819e272efbfbb7ac
matheus.martinez <matheus.martinez@uppertools.inf.br>
2021-08-21 15:29:37 -0300

    Ajuste link ativo

--- src/components/ActiveLink.tsx
+++ src/components/ActiveLink.tsx
@@ -7,19 +7,22 @@ interface ActiveLinkProps extends LinkProps {
   shouldMatchExactHref?: boolean;
 }
 
-export function ActiveLink({ 
-  children, 
-  shouldMatchExactHref = false, 
-  ...rest }: 
-  ActiveLinkProps) {
-  const { asPath} = useRouter();
+export function ActiveLink({
+  children,
+  shouldMatchExactHref = false,
+  ...rest
+}: ActiveLinkProps) {
+  const { asPath } = useRouter();
   let isActive = false;
 
-  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as)) isActive = true;
+  if (shouldMatchExactHref && (asPath === rest.href || asPath === rest.as))
+    isActive = true;
 
-  if (!shouldMatchExactHref && (asPath.startsWith(String(rest.href)) ||
-                                asPath.startsWith(String(rest.as))                              
-                               )) isActive = true;
+  if (
+    !shouldMatchExactHref &&
+    (asPath.startsWith(String(rest.href)) || asPath.startsWith(String(rest.as)))
+  )
+    isActive = true;
 
   return (
     <Link {...rest}>
